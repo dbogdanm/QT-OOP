54b38df8232bdfe263be97c617e45e9f8f67b732
Bogdan <bogdandinu625@gmail.com>
2025-12-04 19:36:11 +0200

    Refactor LoginWindow to use UI file and QDialog

--- FleetEase/loginwindow.cpp
+++ FleetEase/loginwindow.cpp
@@ -1,40 +1,27 @@
-#include "Models/loginwindow.h".h"
-#include <QVBoxLayout>
-#include <QFormLayout>
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+#include "mainwindow.h"
+#include <QMessageBox>
 
-LoginWindow::LoginWindow(QWidget *parent)
-    : QWidget(parent)
-{
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    QFormLayout *formLayout = new QFormLayout();
-
-    usernameEdit = new QLineEdit(this);
-    passwordEdit = new QLineEdit(this);
-    passwordEdit->setEchoMode(QLineEdit::Password); // Parola ascunsă
-
-    formLayout->addRow("Username:", usernameEdit);
-    formLayout->addRow("Password:", passwordEdit);
-
-    loginButton = new QPushButton("Login", this);
-    statusLabel = new QLabel(this);
-
-    mainLayout->addLayout(formLayout);
-    mainLayout->addWidget(loginButton);
-    mainLayout->addWidget(statusLabel);
-
-    connect(loginButton, &QPushButton::clicked, this, &LoginWindow::attemptLogin);
+LoginWindow::LoginWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoginWindow) {
+    ui->setupUi(this);
 }
 
-void LoginWindow::attemptLogin()
-{
-    QString enteredUsername = usernameEdit->text();
-    QString enteredPassword = passwordEdit->text();
+LoginWindow::~LoginWindow() {
+    delete ui;
+}
 
-    if (enteredUsername == validUsername && enteredPassword == validPassword) {
-        // Autentificare reușită
-        emit loginSuccessful();
+void LoginWindow::on_loginButton_clicked() {
+    QString username = ui->usernameLineEdit->text();
+    QString password = ui->passwordLineEdit->text();
+
+    if (username == "admin" && password == "1234") {
+        this->hide();
+        MainWindow *mw = new MainWindow();
+        mw->show();
     } else {
-        // Autentificare eșuată
-        statusLabel->setText("Username sau parolă incorecte!");
+        QMessageBox::warning(this, "Login Failed", "Invalid username or password");
     }
 }
