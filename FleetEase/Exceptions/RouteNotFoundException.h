a8e6f55ac3b855d8b1407cfd99bf1bae00cb4b5a
Bogdan <bogdandinu625@gmail.com>
2025-12-04 19:39:27 +0200

    Update RouteNotFoundException class definition
    Refactor RouteNotFoundException to accept a custom message.

--- FleetEase/Exceptions/RouteNotFoundException.h
+++ FleetEase/Exceptions/RouteNotFoundException.h
@@ -3,26 +3,11 @@
 
 #include "FleetException.h"
 
-/**
- * @class RouteNotFoundException
- * @brief Exception thrown when a route cannot be found in the fleet system.
- *
- * This exception is used to indicate that a specific route, identified by its unique ID,
- * does not exist or cannot be located in the fleet management system.
- */
-class RouteNotFoundException : public FleetException
-{
+using namespace std;
+
+class RouteNotFoundException : public FleetException {
 public:
-    /**
-     * @brief Constructs a RouteNotFoundException with the specified route ID.
-     *
-     * @param routeId The unique ID of the route that could not be found.
-     *
-     * Initializes the exception with a custom error message that includes the
-     * route's ID for debugging purposes.
-     */
-    explicit RouteNotFoundException(int routeId)
-        : FleetException("Route with ID " + std::to_string(routeId) + " not found.") {}
+    explicit RouteNotFoundException(const string& msg) : FleetException(msg) {}
 };
 
 #endif
