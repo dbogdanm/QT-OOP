97f46eaf9abc29ee98ac99582238b25a2023abe7
Bogdan <bogdandinu625@gmail.com>
2025-12-04 19:38:52 +0200

    Refactor FleetException class implementation

--- FleetEase/Exceptions/FleetException.h
+++ FleetEase/Exceptions/FleetException.h
@@ -3,41 +3,17 @@
 
 #include <exception>
 #include <string>
-#include <utility>
 
-/**
- * @class FleetException
- * @brief Base class for all exceptions related to fleet management.
- *
- * The FleetException class extends the standard std::exception class
- * and provides a mechanism to include a custom error message for fleet-related exceptions.
- */
-class FleetException : public std::exception
-{
+using namespace std;
+
+class FleetException : public exception {
 protected:
-    /**
-     * @brief Custom error message for the exception.
-     */
-    std::string message;
+    string message;
 
 public:
-    /**
-     * @brief Constructs a FleetException with a given error message.
-     *
-     * @param msg A string containing the error message to be associated with the exception.
-     */
-    explicit FleetException(std::string msg) : message(std::move(msg)) {}
+    explicit FleetException(const string& msg) : message(msg) {}
 
-    /**
-     * @brief Retrieves the error message associated with the exception.
-     *
-     * @return A C-style string containing the error message.
-     *
-     * This function overrides the `what()` method from std::exception to
-     * provide a custom error message.
-     */
-    [[nodiscard]] const char* what() const noexcept override
-    {
+    virtual const char* what() const noexcept override {
         return message.c_str();
     }
 };
